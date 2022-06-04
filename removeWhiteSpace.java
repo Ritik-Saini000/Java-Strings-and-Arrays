a38cdf5e25aaa77a97f9d1ee16d47dd2ab195885
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-04 20:18:08 +0530

    Add files via upload

---
+++ removeWhiteSpace.java
@@ -0,0 +1,13 @@
+
+public class removeWhiteSpace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1="remove white spaces";
+		
+		str1=str1.replaceAll("\\s+","");
+		System.out.println(str1);
+	}
+
+}
