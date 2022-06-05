ce8055d0bd9fdf1da147e0b79e7f77ba08a2f3d9
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-05 10:59:53 +0530

    Add files via upload

---
+++ DuplicateCharacters.java
@@ -0,0 +1,30 @@
+
+public class DuplicateCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="Great responsibilty";
+		int count;
+		
+		char string[]=str.toCharArray();
+		System.out.println("Duplicate characters in a given string:");
+		
+		for(int i=0;i<str.length();i++)
+		{
+			count=1;
+			for(int j=i+1;j<str.length();j++) 
+			{
+				if(string[i]==string[j] && string[i]!=' ')
+				{
+				count++;
+				string[j]='0';
+				}
+			}
+			
+			if(count>1 && string[i]!='0')
+				System.out.println(string[i]);
+		}
+	}
+
+}
