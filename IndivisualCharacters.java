c6465676583fb46daedc072321c2e695b2467416
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-05 16:21:15 +0530

    Add files via upload

---
+++ IndivisualCharacters.java
@@ -0,0 +1,13 @@
+
+public class IndivisualCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="characters";
+		
+		for(int i=0;i<str.length();i++)
+			System.out.print(str.charAt(i)+" ");
+	}
+
+}
