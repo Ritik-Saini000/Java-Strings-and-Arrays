78af697345b052ba9dac2e0919400e9f258bfbc3
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-08 10:43:18 +0530

    Add files via upload

---
+++ Word_Count.java
@@ -0,0 +1,20 @@
+
+public class Word_Count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="Welcome to java World";
+		int count=1;
+		
+		for(int i=0;i<str.length()-1;i++)
+		{
+			if((str.charAt(i)==' ') && (str.charAt(i+1)!=' '))
+			{
+				count++;
+			}
+		}
+		System.out.println("Words="+count);
+	}
+
+}
