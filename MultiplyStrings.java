7cfc258f4b8cf2525d84349386172ccb63108caa
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-09 11:56:19 +0530

    Add files via upload

---
+++ MultiplyStrings.java
@@ -0,0 +1,13 @@
+
+public class MultiplyStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1="2",str2="3";
+		int num1=Integer.parseInt(str1);
+		int num2=Integer.parseInt(str2);
+		
+		System.out.println("Product="+num1*num2);
+	}
+
+}
